d679abce3ae4bd678c8f60e4f09661f38aaee858
Ravi Soares <broccosoaresravi@gmail.com>
2025-12-30 13:10:24 -0300

    commit correção de credenciais públicas
    eu tinha publicado expondo minhas credenciais do firebase, corrigi movendo elas para variáveis env

--- trabalhos/alunos/src/config/firebaseConfig.ts
+++ trabalhos/alunos/src/config/firebaseConfig.ts
@@ -1,10 +1,10 @@
 import { initializeApp } from "firebase/app";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyCLoCwFL1xDTYNN0ccEwjpA8rbnPD5oSoo",
-  authDomain: "alunoss-fdf2f.firebaseapp.com",
-  projectId: "alunoss-fdf2f",
-  storageBucket: "alunoss-fdf2f.firebasestorage.app",
-  messagingSenderId: "852267003058",
-  appId: "1:852267003058:web:660492bbc5f4967e74edab"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
