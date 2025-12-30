e1042d19013121f067eec03de2a2907a6aa31366
Ravi Soares <broccosoaresravi@gmail.com>
2025-12-30 13:27:21 -0300

    commit correção do vazamento

--- trabalhos/receitas/src/config/firebaseConfig.ts
+++ trabalhos/receitas/src/config/firebaseConfig.ts
@@ -1,10 +1,10 @@
 import { initializeApp } from "firebase/app";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyCQXFOGXPwRCvmY06dUnuqLZ1ombU_QChI",
-  authDomain: "receitas-1091d.firebaseapp.com",
-  projectId: "receitas-1091d",
-  storageBucket: "receitas-1091d.firebasestorage.app",
-  messagingSenderId: "661656294084",
-  appId: "1:661656294084:web:950063cc934e8d7c5a6f91"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
